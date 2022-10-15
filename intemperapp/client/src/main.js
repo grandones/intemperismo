ae762ddacaa7ebc4c4cedb430da1728e25fdff3d
Marcelo Carvalho <marcelo.decarvalho@auxility.be>
2022-10-15 00:34:57 -0300

    Carrousel del intemperie

--- intemperapp/client/src/main.js
+++ intemperapp/client/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import '@splidejs/vue-splide/dist/css/splide.min.css';
 
 createApp(App).use(router).mount('#app');
--- intemperapp/client/src/views/About.vue
+++ intemperapp/client/src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is the crew of the Intempirates, YARRR</h1>
+    <Splide  :options="{ rewind: true }" aria-label="My Favorite Images">
+      <SplideSlide>
+        <img src="@/assets/crew/vitor1.jpg" alt="Sample 1">
+      </SplideSlide>
+      <SplideSlide>
+        <img src="@/assets/crew/marco2.jpg" alt="Sample 1">
+      </SplideSlide>
+      <SplideSlide>
+        <img src="@/assets/crew/marcelo3.jpg" alt="Sample 1">
+      </SplideSlide>
+      <SplideSlide>
+        <img src="@/assets/crew/fern4.jpg" alt="Sample 1">
+      </SplideSlide>
+      <SplideSlide>
+        <img src="@/assets/crew/fafis5.jpg" alt="Sample 1">
+      </SplideSlide>
+    </Splide>
   </div>
 </template>
+
+<script>
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+
+export default {
+  components: {
+    Splide,
+    SplideSlide,
+  },
+};
+</script>
